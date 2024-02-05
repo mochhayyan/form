7cc2d1b828300acff6cc0b715699bac329210d46
mochhayyan <143980747+mochhayyan@users.noreply.github.com>
2024-02-04 18:25:48 -0800

    Add files via upload

---
+++ pertemuan_4/nilai.py
@@ -0,0 +1,12 @@
+nilai = 120
+
+if (nilai > 90 and nilai <= 100):
+    print("Selamat nilai anda Grade A")
+elif (nilai > 80 and nilai <= 90):
+    print("Good job your Grade B")
+elif (nilai >= 75 and nilai <= 80):
+    print("Congratulations your Grade C")
+else:
+    print("Sorry your Grade D")
+else:
+    print("enter the correct value")
---
+++ pertemuan_4/tugas1.py
@@ -0,0 +1,9 @@
+x = 9
+y = 2
+
+if (x > y):
+    print("bilangan pertama lebih besar")
+elif (x < y):
+    print("bilangan kedua lebih besar")
+else:
+    print("kedua bilangan sama")
